c2c57bde0f2685cd51f492c03ac775c9dbf436b0
Kelvin Chung <kelv@kelvSYC.com>
2026-04-30 21:03:21 -0700

    Kotlin JVM library convention

---
+++ gradle/plugins/kotlin-convention/src/main/kotlin/com.kelvsyc.internal.kotlin-tools.kotlin-jvm-library.gradle.kts
@@ -0,0 +1,37 @@
+import kotlin.jvm.optionals.getOrNull
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
+plugins {
+    kotlin("jvm")
+}
+
+val libs = versionCatalogs.named("libs")
+
+kotlin {
+    compilerOptions {
+        apiVersion.set(KotlinVersion.KOTLIN_2_3)
+        languageVersion.set(KotlinVersion.KOTLIN_2_3)
+    }
+
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+java {
+    withSourcesJar()
+}
+
+dependencies {
+    implementation(platform("com.kelvsyc.internal.kotlin-tools:platform"))
+
+    testImplementation(dependencies.platform("com.kelvsyc.internal.kotlin-tools:platform"))
+    libs.findLibrary("kotest-assertions-core").getOrNull()?.let { testImplementation(it) }
+    libs.findLibrary("kotest-assertions-shared").getOrNull()?.let { testImplementation(it) }
+    libs.findLibrary("kotest-framework-engine").getOrNull()?.let { testImplementation(it) }
+    libs.findLibrary("kotest-runner").getOrNull()?.let { testImplementation(it) }
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
