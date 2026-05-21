2e57ed04e594210e5a81c0762edac4d0b4010e9e
Kelvin Chung <kelv@kelvSYC.com>
2026-05-20 18:26:13 -0700

    SortedBiMap extends SortedMap; MutableSortedBiMap adds MutableSortedSet keys and mutable range-view overrides

--- cores/kotlin-core/src/commonMain/kotlin/com/kelvsyc/kotlin/core/collections/MutableSortedBiMap.kt
+++ cores/kotlin-core/src/commonMain/kotlin/com/kelvsyc/kotlin/core/collections/MutableSortedBiMap.kt
@@ -4,5 +4,12 @@ package com.kelvsyc.kotlin.core.collections
  * A mutable [SortedBiMap].
  */
 interface MutableSortedBiMap<K, V> : SortedBiMap<K, V>, MutableBiMap<K, V> {
+    override val keys: MutableSortedSet<K>
     override val inverse: MutableBiMap<V, K>
+
+    override fun headMap(toKey: K, inclusive: Boolean): MutableSortedBiMap<K, V>
+    override fun tailMap(fromKey: K, inclusive: Boolean): MutableSortedBiMap<K, V>
+    override fun subMap(fromKey: K, fromInclusive: Boolean, toKey: K, toInclusive: Boolean): MutableSortedBiMap<K, V>
+    override fun descendingMap(): MutableSortedBiMap<K, V>
+    override fun descendingKeySet(): MutableSortedSet<K>
 }
